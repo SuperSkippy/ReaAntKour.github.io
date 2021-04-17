53ae553ac498564377a32c33256d808d574d9f25
Rea Antoniou-Kourounioti <rea.antkour@gmail.com>
2021-04-17 21:35:26 +0100

    Updates

---
+++ precache-manifest.b77fe7143f2fbef1ab318e1c313c8752.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35174cdb5c65c8e518a15d2cb717af81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13626fd36fe299529d47",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9b07ecc1559f4d2be78d",
+    "url": "/static/css/main.467e710e.chunk.css"
+  },
+  {
+    "revision": "13626fd36fe299529d47",
+    "url": "/static/js/2.9ba2d365.chunk.js"
+  },
+  {
+    "revision": "456711914460e659dfbbbb8bf79f5cfc",
+    "url": "/static/js/2.9ba2d365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b07ecc1559f4d2be78d",
+    "url": "/static/js/main.330d4097.chunk.js"
+  },
+  {
+    "revision": "9f237e1787cf0cb6d195",
+    "url": "/static/js/runtime-main.253ddc65.js"
+  }
+]);
